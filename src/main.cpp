d88416651e2a63e424cf594d7b61a466d29e973d
Patrick Huesmann <info@patrick-huesmann.de>
2019-11-13 07:09:56 +0100

    tried own rotary encoding

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,16 @@ static const int ROTARY_CLK = 3;
 static const int ROTARY_DT = 4;
 
 static void encoder_switch_isr(void);
+static void encoder_rotate_isr(void);
 
 static Encoder rotary(ROTARY_DT, ROTARY_CLK);
 
+static long rotVal = 0;
+
 int getRotaryValue()
 {
-  static long lastEncoderPos = -999;
-  long newEncoderPos = rotary.read();
+  static long lastEncoderPos = 0;
+  long newEncoderPos = rotVal;
   int diff = lastEncoderPos - newEncoderPos;
   if (diff != 0) {
     Serial.println("Alte Position: " + String(lastEncoderPos) + " Neue Position: " + String(newEncoderPos) + " diff: " + String(diff));
@@ -31,6 +34,7 @@ void setup()
 
   pinMode(LED_BUILTIN, OUTPUT);
   attachInterrupt(digitalPinToInterrupt(ROTARY_SW), encoder_switch_isr, FALLING);
+  attachInterrupt(digitalPinToInterrupt(ROTARY_CLK), encoder_rotate_isr, FALLING);
   Serial.begin(115200);
 }
 
@@ -68,5 +72,25 @@ static void encoder_switch_isr(void)
   }
   if(counter >= 5) {
     flag = true;
+  }
+}
+
+static void encoder_rotate_isr(void)
+{
+  int counter_clk = 0;
+  int counter_dt = 0;
+
+  for (int i = 0; i < 100; i++) {
+    if (!digitalRead(ROTARY_CLK)) {
+      counter_clk++;
+    } else {
+      counter_clk = 0;
+    }
+    counter_dt += digitalRead(ROTARY_DT) ? -1 : 1;
+    delayMicroseconds(500);
+  }
+  if (counter_clk >= 20) {
+    rotVal += (counter_dt > 0) ? -1 : 1;
+//    flag = true;
   }
 }
