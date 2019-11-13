24afdd67c9b24e786dbefc66a3f43f971d50ac87
Patrick Huesmann <info@patrick-huesmann.de>
2019-11-13 11:03:05 +0100

    Added autoformatter

--- src/main.cpp
+++ src/main.cpp
@@ -16,26 +16,26 @@ static long rotVal = 0;
 
 int getRotaryValue()
 {
-  static long lastEncoderPos = 0;
-  long newEncoderPos = rotVal;
-  int diff = lastEncoderPos - newEncoderPos;
-  if (diff != 0) {
-    Serial.println("Alte Position: " + String(lastEncoderPos) + " Neue Position: " + String(newEncoderPos) + " diff: " + String(diff));
-    lastEncoderPos = newEncoderPos;
-  }
-  return diff;
+    static long lastEncoderPos = 0;
+    long newEncoderPos = rotVal;
+    int diff = lastEncoderPos - newEncoderPos;
+    if (diff != 0) {
+        Serial.println("Alte Position: " + String(lastEncoderPos) + " Neue Position: " + String(newEncoderPos) + " diff: " + String(diff));
+        lastEncoderPos = newEncoderPos;
+    }
+    return diff;
 }
 
 void setup()
 {
-  pinMode(ROTARY_SW, INPUT_PULLUP);
-  pinMode(ROTARY_CLK, INPUT_PULLUP);
-  pinMode(ROTARY_DT, INPUT_PULLUP);
+    pinMode(ROTARY_SW, INPUT_PULLUP);
+    pinMode(ROTARY_CLK, INPUT_PULLUP);
+    pinMode(ROTARY_DT, INPUT_PULLUP);
 
-  pinMode(LED_BUILTIN, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(ROTARY_SW), encoder_switch_isr, FALLING);
-  attachInterrupt(digitalPinToInterrupt(ROTARY_CLK), encoder_rotate_isr, FALLING);
-  Serial.begin(115200);
+    pinMode(LED_BUILTIN, OUTPUT);
+    attachInterrupt(digitalPinToInterrupt(ROTARY_SW), encoder_switch_isr, FALLING);
+    attachInterrupt(digitalPinToInterrupt(ROTARY_CLK), encoder_rotate_isr, FALLING);
+    Serial.begin(115200);
 }
 
 volatile bool flag = false;
@@ -43,54 +43,54 @@ volatile bool flag = false;
 void loop()
 {
 #if 0
-  static bool x = false;
-  Serial.println("Hello World");
-  delay(500);
+    static bool x = false;
+    Serial.println("Hello World");
+    delay(500);
 
-  digitalWrite(LED_BUILTIN, x ? HIGH : LOW);
-  x = !x;
+    digitalWrite(LED_BUILTIN, x ? HIGH : LOW);
+    x = !x;
 #endif
-  if (flag) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(100);
-    digitalWrite(LED_BUILTIN, LOW);
-    flag = false;
-  }
-  getRotaryValue();
+    if (flag) {
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(100);
+        digitalWrite(LED_BUILTIN, LOW);
+        flag = false;
+    }
+    getRotaryValue();
 }
 
 static void encoder_switch_isr(void)
 {
-  int counter = 0;
-  for(int i = 0; i < 20; i++) {
-    if(digitalRead(ROTARY_SW) == 0) {
-      counter++;
-    } else {
-      counter = 0;
+    int counter = 0;
+    for(int i = 0; i < 20; i++) {
+        if(digitalRead(ROTARY_SW) == 0) {
+            counter++;
+        } else {
+            counter = 0;
+        }
+        delay(4);
     }
-    delay(4);
-  }
-  if(counter >= 5) {
-    flag = true;
-  }
+    if(counter >= 5) {
+        flag = true;
+    }
 }
 
 static void encoder_rotate_isr(void)
 {
-  int counter_clk = 0;
-  int counter_dt = 0;
+    int counter_clk = 0;
+    int counter_dt = 0;
 
-  for (int i = 0; i < 100; i++) {
-    if (!digitalRead(ROTARY_CLK)) {
-      counter_clk++;
-    } else {
-      counter_clk = 0;
+    for (int i = 0; i < 100; i++) {
+        if (!digitalRead(ROTARY_CLK)) {
+            counter_clk++;
+        } else {
+            counter_clk = 0;
+        }
+        counter_dt += digitalRead(ROTARY_DT) ? -1 : 1;
+        delayMicroseconds(500);
     }
-    counter_dt += digitalRead(ROTARY_DT) ? -1 : 1;
-    delayMicroseconds(500);
-  }
-  if (counter_clk >= 20) {
-    rotVal += (counter_dt > 0) ? -1 : 1;
+    if (counter_clk >= 20) {
+        rotVal += (counter_dt > 0) ? -1 : 1;
 //    flag = true;
-  }
+    }
 }
---
+++ style.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+astyle -A3 -s4 -p -xg -n -r -j "src/*.cpp" "include/*.h"
