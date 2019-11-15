a89ed23372359b12a3fb0119ade3a465439c5a2c
Patrick Huesmann <info@patrick-huesmann.de>
2019-11-15 14:13:33 +0100

    Show temperature history

--- src/display.cpp
+++ src/display.cpp
@@ -21,6 +21,10 @@ void display_init()
     } while (display.nextPage());
 }
 
+#define HISTORY_DEPTH 400
+uint8_t temp_hist[HISTORY_DEPTH] = {0};
+uint16_t hist_idx = 0;
+
 void display_redraw()
 {
     static int cnt = 0;
@@ -37,6 +41,14 @@ void display_redraw()
                  + String(T.temp_external, 2)
                  + "°, cnt: "
                  + String(cnt);
+    
+    hist_idx++;
+    hist_idx %= HISTORY_DEPTH;
+    const float t_yw = display.height() - 2 - 30;
+    const float t_scaled = (T.temp_external - 15) / (30 - 15);
+    temp_hist[hist_idx] = display.height() - (uint8_t)(t_scaled * t_yw);
+
+    uint16_t tmp_idx = hist_idx;
 
     display.setPartialWindow(0, 0, display.width(), display.height());
     display.firstPage();
@@ -44,5 +56,10 @@ void display_redraw()
         display.fillScreen(GxEPD_WHITE);
         display.setCursor(10, 15);
         display.print(str);
+        for (uint16_t i = 0; i < HISTORY_DEPTH; i++) {
+            tmp_idx++;
+            tmp_idx %= HISTORY_DEPTH;
+            display.writePixel(i, temp_hist[tmp_idx], GxEPD_BLACK);
+        }
     } while (display.nextPage());
 }
