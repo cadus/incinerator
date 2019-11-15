c1b81fefdf273174fc3e3ed3cbafe729f4cb3900
Patrick Huesmann <info@patrick-huesmann.de>
2019-11-15 18:51:52 +0100

    created scripts directory

---
+++ scripts/style.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+projDir="${scriptDir}/../"
+
+astyle -A3 -s4 -p -xg -n -r -j "src/*.cpp" "include/*.h"
--- style.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-astyle -A3 -s4 -p -xg -n -r -j "src/*.cpp" "include/*.h"
