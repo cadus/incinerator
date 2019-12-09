d696f5d96630dc5202435501464ef53110686444
Patrick Huesmann <info@patrick-huesmann.de>
2019-12-09 18:07:53 +0100

    removed paged drawing

--- src/display.cpp
+++ src/display.cpp
@@ -21,13 +21,11 @@ GxEPD2_BW<GxEPD2_420, GxEPD2_420::HEIGHT> display(GxEPD2_420(EPD_CS, EPD_DC, EPD
 
 void display_init()
 {
-    display.init(115200);
+    display.init();
     display.setRotation(0);
     display.setFullWindow();
-    display.firstPage();
-    do {
-        display.fillScreen(GxEPD_WHITE);
-    } while (display.nextPage());
+    display.fillScreen(GxEPD_WHITE);
+    display.display(false); // full update
 }
 
 void display_redraw()
@@ -53,17 +51,14 @@ void display_redraw()
 
     display.setPartialWindow(0, 0, display.width(), display.height());
     unsigned long start = micros();
-    display.firstPage();
-    do {
-        display.fillScreen(GxEPD_WHITE);
-        display.setCursor(10, 15);
-        display.print(str);
-        for (uint8_t i = 0; i < 25; i++) {
-            const uint8_t *icon = (ep > i) ? ICON_BOX_FILLED_DATA : ICON_BOX_CLEAR_DATA;
-            display.drawBitmap(i * ICON_BOX_CLEAR_WIDTH, 20, icon, ICON_BOX_CLEAR_WIDTH, ICON_BOX_CLEAR_HEIGHT, GxEPD_BLACK);
-        }
-        GxEPD2_busyWaitCallback();
-    } while (display.nextPage());
+    display.fillScreen(GxEPD_WHITE);
+    display.setCursor(10, 15);
+    display.print(str);
+    for (uint8_t i = 0; i < 25; i++) {
+        const uint8_t *icon = (ep > i) ? ICON_BOX_FILLED_DATA : ICON_BOX_CLEAR_DATA;
+        display.drawBitmap(i * ICON_BOX_CLEAR_WIDTH, 20, icon, ICON_BOX_CLEAR_WIDTH, ICON_BOX_CLEAR_HEIGHT, GxEPD_BLACK);
+    }
+    display.display(true); // partial update
     unsigned long elapsed = micros() - start;
     Serial.print("time spent - w/o refresh: ");
     Serial.print(elapsed - 560000);
