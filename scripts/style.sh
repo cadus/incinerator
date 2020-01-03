f965daf3ee6fd9d1412a112921f14deb70578e9e
Patrick Huesmann <info@patrick-huesmann.de>
2020-01-03 18:58:37 +0100

    fix include directory for code style script

--- scripts/style.sh
+++ scripts/style.sh
@@ -3,4 +3,4 @@
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 projDir="${scriptDir}/../"
 
-astyle -A3 -s4 -p -xg -n -r -j "src/*.cpp" "include/*.h"
+astyle -A3 -s4 -p -xg -n -r -j "src/*.cpp" "src/*.h"
