60d744ec9fdbad50ff0a51c9f9439a1c22f9d137
Patrick Huesmann <info@patrick-huesmann.de>
2020-12-23 12:03:09 +0100

    Add GitHub workflow

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Firmware & electronics for the CADUS incinerator
+![PlatformIO CI](https://github.com/cadus/incinerator/workflows/PlatformIO%20CI/badge.svg)
+
 Furnace for the incineration of medical waste.
 
 ## Project Docs
--- gitversion.py
+++ gitversion.py
@@ -3,17 +3,21 @@
 import subprocess
 from subprocess import PIPE
 import sys
+import os
 import datetime
+import re
 
 git_cmd = [
     "git",
-    "--no-pager",
-    "describe",
+    "--no-pager", "describe",
     "--dirty",
     "--always",
     "--tags",
-    "--abbrev=8"
+    "--match=[vV]*.*",
+    "--abbrev=8",
+    "--long"
 ]
+re_git_ver = re.compile(r"^[vV]0*([0-9]+).0*([0-9]+)-([0-9]+)-g([0-9a-f]+)-?(dirty)?$")
 
 git_cmd_result = subprocess.run(git_cmd, stdout=PIPE, stderr=PIPE)
 
@@ -26,6 +30,27 @@ gitversion = gitversion.decode('utf-8')
 
 print(f"Git version: {gitversion}")
 
+match = re_git_ver.match(gitversion)
+if not match:
+    print(f"Cannot parse Git version ({gitversion})")
+    sys.exit(-1)
+
+ver_major, ver_minor, num_commits_since_tag, commit_hash, dirty_flag = match.groups()
+ver_major = int(ver_major)
+ver_minor = int(ver_minor)
+num_commits_since_tag = int(num_commits_since_tag)
+ver_txt = f"V{ver_major}.{ver_minor:02d}"
+if num_commits_since_tag:
+    ver_txt += f"-{num_commits_since_tag}"
+if dirty_flag:
+    ver_txt += f"-{dirty_flag}"
+
+progname = f"incinerator-{ver_txt}"
+if "Import" in globals():
+    # Set binary file name for PlatformIO build
+    Import("env")
+    env.Replace(PROGNAME=progname)
+
 builddate = datetime.datetime.utcnow()
 builddate = builddate.strftime("%F %T")
 
@@ -33,5 +58,11 @@ print(f"Build date: {builddate}")
 
 with open("src/version.cpp", "w") as ofile:
     ofile.write(f'#include "version.h"\n\n')
-    ofile.write(f'const char gitversion[] = "{gitversion}";\n')
+    ofile.write(f'const char gitversion[] = "{ver_txt}";\n')
     ofile.write(f'const char builddate[] = "{builddate}";\n')
+
+gh_env = os.getenv("GITHUB_ENV")
+if gh_env:
+    # If we run on GitHub CI, export the binary's name for CI script
+    with open(gh_env, "a") as f:
+        f.write("BIN_NAME=" + progname + ".bin\n")
